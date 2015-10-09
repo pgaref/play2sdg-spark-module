f4cff36fe3a52001e7d2b5aee73a7770dee566fd
Panagiotis Garefalakis <pangaref@gmail.com>
2015-10-09 20:33:13 +0100

    Data loader to use new io

--- src/main/java/uk/ac/imperial/lsds/play2sdg/PrepareData.java
+++ src/main/java/uk/ac/imperial/lsds/play2sdg/PrepareData.java
@@ -1,17 +1,19 @@
 package main.java.uk.ac.imperial.lsds.play2sdg;
 
+
+import org.apache.log4j.Logger;
+
+import com.datastax.driver.core.Session;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-import main.java.uk.ac.imperial.lsds.cassandra.CassandraQueryController;
+import main.java.uk.ac.imperial.lsds.dx_controller.CassandraDxQueryController;
+import main.java.uk.ac.imperial.lsds.dx_models.PlayList;
+import main.java.uk.ac.imperial.lsds.dx_models.Track;
+import main.java.uk.ac.imperial.lsds.dx_models.User;
 import main.java.uk.ac.imperial.lsds.io_handlers.UserFileParser;
 import main.java.uk.ac.imperial.lsds.io_handlers.LastFMDataParser;
-import main.java.uk.ac.imperial.lsds.models.PlayList;
-import main.java.uk.ac.imperial.lsds.models.Track;
-import main.java.uk.ac.imperial.lsds.models.User;
 
 
 public class PrepareData {
@@ -20,11 +22,12 @@ public class PrepareData {
 	/*
 	 * TODO: pass these Values as arguments 
 	 */
-	private static String dataset_path = "hdfs://wombat30.doc.res.ic.ac.uk:8020/user/pg1712/lastfm_train";
-	private static String userdata_path = "hdfs://wombat30.doc.res.ic.ac.uk:8020/user/pg1712/users.txt";
+//	private static String dataset_path = "hdfs://wombat30.doc.res.ic.ac.uk:8020/user/pg1712/lastfm_train";
+//	private static String userdata_path = "hdfs://wombat30.doc.res.ic.ac.uk:8020/user/pg1712/users.txt";
 	
-//	private static String dataset_path = "data/LastFM/lastfm_subset";
-//	private static String userdata_path = "data/users.txt";
+	private static String dataset_path = "data/LastFM/lastfm_subset";
+	private static String userdata_path = "data/users.txt";
+	private CassandraDxQueryController queryController;
 	
 	private static Logger  logger = Logger.getLogger(PrepareData.class);
 	private List<User> users;
@@ -32,21 +35,39 @@ public class PrepareData {
 	private List<PlayList> playlists;
 	
 	
+	public PrepareData(Session clusterSession) {
+		queryController = new CassandraDxQueryController(clusterSession);
+		users = this.prepareUsers();
+		tracks = this.prepareTracks();
+		playlists = this.preparePlayLists(20);
+	}
+	
+	public PrepareData(String userpath, String datapath, Session clusterSession) {
+		queryController = new CassandraDxQueryController(clusterSession);
+		dataset_path = datapath;
+		userdata_path = userpath;
+		users = this.prepareUsers();
+		tracks = this.prepareTracks();
+		playlists = this.preparePlayLists(20);
+	}
+	
 	private List<User> prepareUsers(){	
 		UserFileParser userParser = new UserFileParser(userdata_path);
 		List<User> users = userParser.ParseUsers();
 		logger.debug("Persisting User Data! ### ");
 		for(User u : users){
-			System.out.println("\n Read user: "+ u);
-			CassandraQueryController.persist(u);
-			System.out.println("Persisted ...");
+			logger.debug("\n Read user: "+ u);
+			queryController.persist(u);
+			logger.debug("Persisted ...");
 		}
 		return users;
 	}
 	
 	private List<Track> prepareTracks(){
 		LastFMDataParser parser = new LastFMDataParser(dataset_path);
-		List<Track> tracks = parser.parseDataSet(true);
+		List<Track> tracks = parser.parseDataSet();
+		for(Track t : tracks)
+			queryController.persist(t);
 		logger.debug("Sucessfully persisted #"+ tracks.size() + "# Tracks" );
 		return tracks;
 		
@@ -60,10 +81,10 @@ public class PrepareData {
 			
 			for(int i = 0 ; i < NumOfTracks ; i ++ ){
 				Track t = this.tracks.get( (int)(Math.random()* this.tracks.size()) );
-				plist.addRatingSong(t);
+				plist.addRatingSong(t.getTitle());
 			}
 			plists.add(plist);
-			CassandraQueryController.persist(plist);
+			queryController.persist(plist);
 			logger.debug("Persisted PlayList: "+plist.toString() +" for user: " +u.getEmail()+" ### ");
 			
 			
@@ -72,28 +93,17 @@ public class PrepareData {
 		return plists;
 	}
 	
-	public PrepareData() {
-		
-		this.users = prepareUsers();
 
-		this.tracks = prepareTracks();
-		/*
-		 * Configurable number of random songs in the playlist
-		 */
-		this.playlists = preparePlayLists(20);
-		
-	}
-	
-	public static void main(String[] args) {
-		
-		/*
-		 * Configurable Arguments so far: 1)UserFile 2)TracksFile 3)Number of tracks in default Playlist
-		 */
-		logger.setLevel(Level.DEBUG);
-		logger.debug("Initating Prepare  Data for paths: Data path: " + dataset_path + " - User path:  "+ userdata_path+" ### ");
-		PrepareData p = new PrepareData();
-		logger.debug("Finished Preparing  Data for paths: Data path: " + dataset_path + " - User path:  "+ userdata_path+" ### ");
-	}
+//	public static void main(String[] args) {
+//		
+//		/*
+//		 * Configurable Arguments so far: 1)UserFile 2)TracksFile 3)Number of tracks in default Playlist
+//		 */
+//		logger.setLevel(Level.DEBUG);
+//		logger.debug("Initating Prepare  Data for paths: Data path: " + dataset_path + " - User path:  "+ userdata_path+" ### ");
+//		PrepareData p = new PrepareData();
+//		logger.debug("Finished Preparing  Data for paths: Data path: " + dataset_path + " - User path:  "+ userdata_path+" ### ");
+//	}
 	
 	
 
